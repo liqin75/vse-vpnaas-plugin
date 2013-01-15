eea170067eecfba5e5b25f1835d0d0b10f9db990
Kaiwei Fan <kaiwei.fan@gmail.com>
2013-01-15 10:54:58 -0800

    enable LB, delete pool

--- quantum/plugins/vmware/vshield/vseapi.py
+++ quantum/plugins/vmware/vshield/vseapi.py
@@ -13,24 +13,27 @@ class VseAPI():
         self.edgeInstance = edgeInstance
         self.configId = 0
 
-    def rawapi(self, method, uri, params=None):
+    def vsmconfig(self, method, uri, params=None):
         print "VseAPI('{}', '{}', '{}')".format(method, uri, json.dumps(params))
         resp, content = self.vsmapi.api(method, uri, params)
         print "header: '{}'".format(resp)
         print "content: '{}'".format(content)
         if int(resp['status']) / 100 != 2:
             raise Exception(json.dumps(resp) + "\n" + content)
-
-        config = self.get_vse_config()
-        self.configId = self.configId + 1
-        self.osapi.inject_file(self.edgeInstance, '/root/vSEdge/os-inject/config.json', self.configId, config)
-        return resp, content
-
-    def api(self, method, uri, params=None):
-        resp, content = self.rawapi(method, uri, params)
         if content == '':
             return {}
         return json.loads(content)
+
+    def vsm2os(self):
+        config = self.get_vse_config()
+        self.configId = self.configId + 1
+        self.osapi.inject_file(self.edgeInstance, '/root/vSEdge/os-inject/config.json', self.configId, config)
+
+    def api(self, method, uri, params=None):
+        content = self.vsmconfig(method, uri, params)
+        if method.upper() is not "GET":
+            self.vsm2os()
+        return content
 
     def get_edgeId(self):
         return self.edgeId
